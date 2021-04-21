ec2637a5ee471d42197530662c904ebdc6fdfb3b
midori56green <58257763+midori56green@users.noreply.github.com>
2021-04-22 03:11:17 +0900

    push

--- event_x/1_0329/test_index/script.js
+++ event_x/1_0329/test_index/script.js
@@ -1,8 +1,14 @@
-const scrollBtn = document.getElementsByClassName('scroll-btn');
-console.log(window.height());
+const scrollBtn = document.getElementById('scroll-btn');
+const mainContent = document.getElementById('main');
 
 // click * .scroll-btn
-scrollBtn.addEventListener('click', (e) => {
-	e.preventListener(e);
-	console.log('click');
+scrollBtn.addEventListener('click', function(e) {
+	e.preventDefault(e);
+	const offset = window.pageYOffset;
+	const target_position = mainContent.getBoundingClientRect().top;
+	const position = offset + target_position;
+	window.scrollTo({
+		top: position,
+		behavior: 'smooth'
+	});
 });
